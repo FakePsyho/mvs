abb156cd583f7a3efae5330d1d73269f03c4ff9e
Psyho <closedeyez@gmail.com>
2017-01-29 23:55:31 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-g++ main.cpp -O2 -std=gnu++11 -pthread -lpthread -w -o x -lX11
+g++ main.cpp -O2 -std=gnu++11 -pthread -lpthread -w -o mvs
+#for visualization-enabled version use this:
+#g++ main.cpp -O2 -std=gnu++11 -pthread -lpthread -w -o mvs -DUSE_VIS -lX11 
